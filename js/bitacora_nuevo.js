337758d3b648cd7bd4cdced11fcd2198dbd06cb7
LuisFer252 <Itenorioa1800@alumno.ipn.mx>
2020-04-26 23:58:26 -0500

    validacion archivo

--- js/bitacora_nuevo.js
+++ js/bitacora_nuevo.js
@@ -1,13 +1,17 @@
 
 $("#btn-mas-img").addEventListener("click", function () {
-   var input = document.createElement("input");
-   input.setAttribute("type", "file");
-   input.setAttribute("required", "true");
-   input.setAttribute("accept", "images/*");
-   input.setAttribute("class", "form-control-file");
-   input.setAttribute("name", "image");
-   var f = document.getElementById("imagenes-bitacora");
-   f.appendChild(input);
+    if (document.getElementsByName("image").length <= 10) {
+        var input = document.createElement("input");
+        input.setAttribute("type", "file");
+        input.setAttribute("required", "true");
+        input.setAttribute("accept", "images/*");
+        input.setAttribute("class", "form-control-file");
+        input.setAttribute("name", "image");
+        var f = document.getElementById("imagenes-bitacora");
+        f.appendChild(input);
+    }else{
+        alert("No se puden enviar mas de diez imagenes por entrada")
+    }
 });
 
 function $(selector){
