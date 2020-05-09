cf87bc9131ae03fabf4ed00dc541c1671af2e4ab
Armando5347 <masterchiefgama123@gmail.com>
2020-05-09 16:50:11 -0500

    Netbins y ya
    En teoría con esto, solo me queda el repo de Netbeans par auq eya se meta todo aquí adentro

--- js/bitacora_eliminar.js
+++
@@ -1,8 +0,0 @@
-
-var iconos = Array.prototype.slice.call(document.getElementsByClassName("fa-times"));
-
-iconos.forEach(element => {
-    element.addEventListener("click", function () {
-        element.parentNode.parentNode.remove();
-    })
-});
